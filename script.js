cbd8ee121bddf0dba615ff27a6ff12366af166af
Alireza <alirezwa03@gmail.com>
2022-06-08 16:58:48 +0430

    creat a variaible

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log('Hello world')
 
-const x = document.querySelector('p').innerHTML = 'Hello world'
+const x = document.querySelector('p').innerHTML = 'Hello world';
+const y = document.querySelector('p').innerHTML = 'Hello world';
